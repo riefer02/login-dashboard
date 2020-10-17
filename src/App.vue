57e82286faf072e017f745f1cd3a4b69c64e5ab7
riefer02 <andrew.riefenstahl@gmail.com>
2020-10-17 08:46:24 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Login Dashboard </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-content>
+      <!-- Login Module -->
+      <v-card width="400px" class="mx-auto mt-10"
+        ><v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Username"
+              prepend-icon="mdi-account-circle"
+            ></v-text-field>
+            <v-text-field
+              :type="showPassword ? 'true' : 'password'"
+              @click:append="showPassword = !showPassword"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye ' : 'mdi-eye-off'"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    showPassword: false,
+    links: ["Home", "Login"],
+  }),
+};
+</script>
